072deb13c247c7136ba61683e21a6761ad5c46d1
Chris Barker <cbarker@puppetlabs.com>
2013-10-16 09:52:19 -0700

    add pe_healthcheck tag

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,9 @@
 class pqsql_usage {
 
   if $pqsql_percentage >= 80 {
-    notify { "You have ${pqsql_percentage} free for your PostGreSQL stor": }
+    notify { "You have ${pqsql_percentage} free for your PostGreSQL stor":
+      tag => pe_healthcheck,
+    }
   }
 
 }
